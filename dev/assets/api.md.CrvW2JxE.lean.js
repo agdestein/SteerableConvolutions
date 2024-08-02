45382aebdb595a1987d3b063f525bb9e2d440026
Documenter.jl <documenter@juliadocs.github.io>
2024-08-02 18:42:30 +0000

    build based on 243dd92

---
+++ dev/assets/api.md.CrvW2JxE.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o,a7 as t}from"./chunks/framework.CKldmBHj.js";const u=JSON.parse('{"title":"API reference","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),a={name:"api.md"},n=t("",3),l=[n];function s(i,c,d,p,_,b){return o(),r("div",null,l)}const h=e(a,[["render",s]]);export{u as __pageData,h as default};
